14a6ccd5939567a9cd03aa09a3979670a32940ad
JoaoSilva1711 <joaomm.silva@hotmail.com>
2022-11-15 21:15:24 +0000

    changes on streamUtils

--- PG3_TRAB02/src/grupo01/StreamsUtils.java
+++ PG3_TRAB02/src/grupo01/StreamsUtils.java
@@ -1,7 +1,29 @@
 package grupo01;
 
-public class StreamsUtils {
-    public StreamsUtils(){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
 
+public class StreamsUtils {
+
+    public static boolean validate( Reader in ) throws IOException {
+
+        /** The reader that is being read. */
+        BufferedReader bufferedReader = new BufferedReader(in);
+        String line = bufferedReader.readLine();
+
+       if(line!=null){
+            int index= line.indexOf("{");
+                if(index!=-1){
+                    String str = line.substring(index+1);
+                    int secondIndex= str.indexOf("}");
+                    if(secondIndex==-1) return false;
+                }
+           line= bufferedReader.readLine();
+       }else{
+           throw new IOException("cannot read from file");
+       }
+       return true;
     }
+
 }
